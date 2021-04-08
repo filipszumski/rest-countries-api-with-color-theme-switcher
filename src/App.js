fe29c335ecd11c0c9dbf8602db7ef1aac3dd9f0d
Filip Szumski <filipszumski@gmail.com>
2021-04-08 23:53:10 +0200

    added HTML structure and basic styling to body

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      blablabla
+      <header>
+        <h1>Where is the world?</h1>
+        <span>Lightn Mode</span>
+      </header>
+      <main>
+        <section>
+          <p>
+            <input placeholder='Seach for a country...' disabled name="country" type="text" />
+          </p>
+          <p>
+            <select name="region">
+              <option value="" disabled selected hidden>Filter by Region</option>
+              <option value="africa">Africa</option>
+              <option value="america">America</option>
+              <option value="asia">Asia</option>
+              <option value="europe">Europe</option>
+              <option value="oceania">Oceania</option>
+            </select>
+          </p>
+        </section>
+        <section>
+
+        </section>
+      </main>
     </div>
   );
 }

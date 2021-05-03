f395d82d65dedeb3ddcf3b8c443c8465da1bdd3f
Filip Szumski <filipszumski@gmail.com>
2021-05-03 22:51:10 +0200

    made function to fetch data from

---
+++ src/services/countries.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const fetchCountriesInfo = async () => {
+    const response = await axios.get("https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;region;subregion;population;borders;nativeName;currencies;languages;flag");
+    return response.data;
+}
+
+

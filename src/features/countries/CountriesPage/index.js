bd6f5f9d6bb2c897a29968c941735ddece7190a0
Filip Szumski <filipszumski@gmail.com>
2021-05-04 22:37:40 +0200

    removed unnecessary React Fragment

--- src/features/countries/CountriesPage/index.js
+++ src/features/countries/CountriesPage/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Main } from "./Main";
+import { Main } from "../../../common/Main";
 import { Filters } from "./Filters";
 import { Countries } from "./Countries";
 
 export function CountriesPage({ countriesData }) {
 
   return (
-    <>
-      <Main>
-        <Filters />
-        <Countries countriesData={countriesData} />
-      </Main>
-    </>
+    <Main>
+      <Filters />
+      <Countries countriesData={countriesData} />
+    </Main>
   );
 }

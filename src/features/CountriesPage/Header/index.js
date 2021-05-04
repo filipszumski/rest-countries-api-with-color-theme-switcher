189403cc19a76492b0c6de7c881f563b20a0fc8d
Filip Szumski <filipszumski@gmail.com>
2021-05-04 22:15:02 +0200

    moved Header component to common folder

--- src/features/CountriesPage/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StyledHeader } from "./styled";
-
-export const Header = ({ title }) => (
-    <StyledHeader>
-        <h1>{title}</h1>
-        <span className="header__toggleThemeButton">Ligth Mode</span>
-    </StyledHeader>
-);

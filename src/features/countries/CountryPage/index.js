d568a0caefb9cb1910d14fca8e1c0e6aec976338
Filip Szumski <filipszumski@gmail.com>
2021-05-04 23:20:02 +0200

    created HTML structure for CoutryPage

--- src/features/countries/CountryPage/index.js
+++ src/features/countries/CountryPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router";
+import { Main } from "../../../common/Main";
 
 
 export const CountryPage = ({ countriesData }) => {
@@ -13,7 +14,32 @@ export const CountryPage = ({ countriesData }) => {
     return (
         <>
             {countriesData.state === "success" && (
-                <h1>{selectedCountry.name}</h1>
+                <Main>
+                    <section>
+                        <button>Back</button>
+                    </section>
+                    <section>
+                        <div>
+                            <img src={selectedCountry.flag} alt={`${selectedCountry.name} Flag`}></img>
+                        </div>
+                        <div>
+                            <h2>{selectedCountry.name}</h2>
+                            <div>
+                                <p><span>Native Name: </span>{selectedCountry.nativeName}</p>
+                                <p><span>Population: </span>{selectedCountry.population}</p>
+                                <p><span>Region: </span>{selectedCountry.region}</p>
+                                <p><span>Sub Region: </span>{selectedCountry.subregion}</p>
+                                <p><span>Capital: </span>{selectedCountry.capital}</p>
+                                <p><span>Native Top Level Domain: </span>{selectedCountry.topLevelDomain}</p>
+                                <p><span>Currencies: </span>{selectedCountry.nativeName}</p>
+                                <p><span>Languages: </span>{selectedCountry.nativeName}</p>
+                            </div>
+                            <div>
+                                <p><span>Border Countires: </span></p>
+                            </div>
+                        </div>
+                    </section>
+                </Main>
             )}
         </>
     )

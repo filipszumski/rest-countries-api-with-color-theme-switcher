f7c4f39459c885290e80fe84b1fb29748f5f595e
Filip Szumski <filipszumski@gmail.com>
2021-05-05 22:06:48 +0200

    added icons and button styled-component styling with icons

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import { StyledHeader, HeaderTitle, StyledButton } from "./styled";
 
 export const Header = ({ title, themeName, toggleThemeName }) => {
+    console.log(themeName);
 
     return (
         <StyledHeader>
             <HeaderTitle>{title}</HeaderTitle>
-            <StyledButton onClick={() => toggleThemeName()}>{themeName}&nbsp;Theme</StyledButton>
+            <StyledButton onClick={() => toggleThemeName()}>
+                {themeName === "Dark"
+                    ? <ion-icon size="small" name="sunny-outline"></ion-icon>
+                    : <ion-icon size="small" name="moon-outline"></ion-icon>}
+                {" "}
+                <span>{themeName}&nbsp;Theme</span>
+            </StyledButton>
         </StyledHeader>
     )
 };
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -7,14 +7,18 @@ export const StyledHeader = styled.header`
 `;
 
 export const HeaderTitle = styled.h1`
-    font-size: ${({theme}) => theme.fontSize.title}px;
+    font-size: ${({ theme }) => theme.fontSize.title}px;
 `;
 
 export const StyledButton = styled.button`
-    padding: 10px;
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    gap: 7px;
+    align-items: center;
+    margin-left: 10px;
+    padding: 7px;
     color: ${({ theme }) => theme.colors.text};
     background-color: ${({ theme }) => theme.colors.elements};
     border: none;
     box-shadow: 1px 1px 5px 0px ${({ theme }) => theme.colors.input};
-
 `;

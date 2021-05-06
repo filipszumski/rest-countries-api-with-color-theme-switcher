536bf2a6523dd388158a68fe807536d98f09ba36
Filip Szumski <filipszumski@gmail.com>
2021-05-06 21:57:48 +0200

    added new state selectors for Country component

--- src/features/countries/CountryPage/index.js
+++ src/features/countries/CountryPage/index.js
@@ -8,9 +8,15 @@ import { Main } from "../../../common/Main";
 export const CountryPage = ({ countriesData }) => {
     const { name } = useParams();
     let selectedCountry;
+    let selectedCountryLaguages;
+    let selectedCountryBordersNames;
 
     if (countriesData.state === "success") {
         selectedCountry = countriesData.countries.find(country => country.name === name);
+        selectedCountryLaguages = selectedCountry.languages.map(language => language.name);
+        selectedCountryBordersNames = selectedCountry.borders
+            .map(countryShotName => countriesData.countries
+                .find(country => country.alpha3Code === countryShotName).name)
     };
 
     return (
@@ -18,7 +24,11 @@ export const CountryPage = ({ countriesData }) => {
             {countriesData.state === "success" && (
                 <Main>
                     <Button />
-                    <Country selectedCountry={selectedCountry} />
+                    <Country
+                        selectedCountry={selectedCountry}
+                        selectedCountryLaguages={selectedCountryLaguages}
+                        selectedCountryBordersNames={selectedCountryBordersNames}
+                    />
                 </Main>
             )}
         </>

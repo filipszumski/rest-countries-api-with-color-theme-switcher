1ea7134dbedefd0f4826836f5a848293eae46e8c
Filip Szumski <filipszumski@gmail.com>
2021-05-06 21:59:43 +0200

    added alpha3Code in url parameter and cached new verios of json

--- src/services/countries.js
+++ src/services/countries.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchCountriesInfo = async () => {
-    const response = await axios.get("https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;region;subregion;population;borders;nativeName;currencies;languages;flag");
+    const response = await axios.get("https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;region;subregion;population;borders;nativeName;currencies;languages;flag;alpha3Code");
     return response.data;
 }
 

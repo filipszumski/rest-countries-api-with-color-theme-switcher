bc2bb2f4f43fa2750e4c0b877f19f0e340bab151
Filip Szumski <filipszumski@gmail.com>
2021-05-08 12:46:36 +0200

    added media queries for Filters Component

--- src/features/countries/CountriesPage/Filters/styled.js
+++ src/features/countries/CountriesPage/Filters/styled.js
@@ -2,6 +2,7 @@ import styled, { css } from "styled-components";
 
 export const Wrapper = styled.section`
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
     margin: 30px;
 `;
@@ -14,6 +15,15 @@ export const Input = styled.input`
     box-shadow: 1px 1px 5px 0px ${({ theme }) => theme.colors.input};
 
     ${({ search }) => search && css`
-        flex: 0 1 300px
+        flex: 0 1 200px;
+        margin-right: 20px;
     `}
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+        flex: 1 0 100%;
+       
+        ${({ search }) => search && css`
+        margin-bottom: 30px;
+    `}
+    }
 `;

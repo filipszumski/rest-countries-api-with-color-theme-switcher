bec6f2ff4bb5b126710600b2bf3dc82e4d3dc5ad
Filip Szumski <filipszumski@gmail.com>
2021-05-09 19:55:39 +0200

    installed react-loading library and created Loding component

---
+++ src/common/Loading/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyledLoading } from "./styled";
+
+export const Loading = () => (
+    <StyledLoading type="spinningBubbles" color={"black"} height={"5%"} width={"5%"} />
+)
+
---
+++ src/common/Loading/styled.js
@@ -0,0 +1,6 @@
+import ReactLoading from "react-loading";
+import styled from "styled-components";
+
+export const StyledLoading = styled(ReactLoading)`
+    flex: 1;
+`

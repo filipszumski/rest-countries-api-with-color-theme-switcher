c942b4ec4af69e57f61dfab81ccbef492c75a8f9
Filip Szumski <filipszumski@gmail.com>
2021-05-13 00:11:00 +0200

    added new button to go back in URL

--- src/features/countries/CountryPage/Button/index.js
+++ src/features/countries/CountryPage/Button/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Wrapper, StyledButton } from "./styled";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { toCountries } from "../../../../routes";
 
-export const Button = () => (
-    <Wrapper>
-        <Link to={toCountries()}><StyledButton>Back</StyledButton></Link>
-    </Wrapper>
-)
+export const Button = () => {
+    const history = useHistory();
+    const onClick = () => {
+        history.goBack();
+    }
+
+    return (
+        <Wrapper>
+            <Link to={toCountries()}><StyledButton>Home</StyledButton></Link>
+            <StyledButton onClick={onClick}>Back</StyledButton>
+        </Wrapper>
+    )
+}
